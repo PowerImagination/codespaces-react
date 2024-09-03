5d06082ce61aa223ee2b5eedfa85c0b24dff0fa7
Hicham <ezzhic@gmail.com>
2024-09-03 14:34:25 +0000

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Home from "./Home";
+import itemcard from './itemcard';
 function App() {
   return (
     <>
+    <Home/>
+    <itemcard/>
     </>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+        <h1 className="text-center mt-3"> All Items</h1>
+        <section className="py-4 container">
+        <div className="row justify-content-center">
+           
+        </div>
+        </section>
+        </>
+    );
+};
+
+export default Home;
