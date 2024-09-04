73d399c0214f4bd265092acea1c77d5abcfc9830
Hicham <ezzhic@gmail.com>
2024-09-04 15:16:40 +0000

    Totale price + Clear cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -40,6 +40,12 @@ const Cart = () => {
                 </tbody>
             </table>
             </div>
+            <div className='col-auto ms-auto'>
+            <h2>Totale Price ${cartTotal}</h2>
+            </div>
+            <div className='col-auto'>
+            <button className='btn btn-danger m-2' onClick={()=> emptyCart()}>Clear Cart</button>
+            </div>
            </div>
             </section>
       
