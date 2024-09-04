21f9193603c4f1666156fcc3bfe92d5cf0dba6f3
Hicham <ezzhic@gmail.com>
2024-09-04 09:00:16 +0000

    some change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from "./Home";
-//import itemcard from './Itemcard';
+import Cart from "./Cart"
+import { CartProvider } from 'react-use-cart';
+
 function App() {
   return (
     <>
+    <CartProvider>
     <Home/>
-
+    <Cart/>
+    </CartProvider>
+   
     </>
   );
 }
