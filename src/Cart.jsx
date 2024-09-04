b95de469c376d3bd3b7b9805e6e4625cfb120a20
Hicham <ezzhic@gmail.com>
2024-09-04 14:27:23 +0000

    add + remove

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useCart } from 'react-use-cart';
+
 const Cart = () => {
     const {
         isEmpty,
@@ -11,18 +12,30 @@ const Cart = () => {
         removeItem,
         emptyCart,
     } = useCart();
-    if (isEmpty) return <p className='text-center'>Your cart is empty</p>;
+    if (isEmpty) return <h1 className="text-center">Your cart is empty</h1>;
     return (
-        <section className='py-4 container'>
-          <div className='row justify-content-center'>
-            <div className='col-12'>
+        <section className="py-4 container">
+          <div className="row justify-content-center">
+            <div className="col-12">
             <h5> Cart({totalUniqueItems}) total Items: ({totalItems})</h5>
-            <table className='table table-light table-hover m-0'>
+            <table className="table table-light table-hover m-0">
+                <tbody>
                 {items.map((item, index)=>{
                     <tr key={index}>
-
+                    <td>
+                        <img src={item.img} style={{height: "6rem"}}/>
+                    </td>
+                    <td>{item.title}</td>
+                    <td>{item.price}</td>
+                    <td>Quantity({item.Quantity})</td>
+                    <td>
+                        <button className="btn btn-info ms-2" onClick={()=> updateItemQuantity(item.id, item.Quantity -1)}>-</button>
+                        <button className="btn btn-info ms-2" onClick={()=> updateItemQuantity(item.id, item.Quantity +1)}>+</button>
+                        <button className="btn btn-danger ms-2" onClick={()=> removeItem(item.id)}>Remove Item</button>
+                    </td>
                     </tr>
                 })}
+                </tbody>
             </table>
             </div>
            </div>
