ad595548e36f0b5a3b60fe4ea581a5d04890f033
Hicham <ezzhic@gmail.com>
2024-09-04 06:51:57 +0000

    product data

---
+++ src/Data.jsx
@@ -0,0 +1,32 @@
+import img1 from './img/shoe1.jpg'
+import img2 from './img/shoe2.jpg'
+import img3 from './img/shoe3.jpg'
+
+const Data = {
+    
+    productData:[
+        {
+            id:1,
+            img: img1,
+            title:'Brawn Shoe',
+            desc:'',
+            price: 100,
+    },
+    {
+        id:2,
+        img: img2,
+        title:'Black Shoe',
+        desc:'',
+        price: 100,
+},
+{
+    id:3,
+    img: img3,
+    title:'Brawn Shoe',
+    desc:'',
+    price: 100,
+}
+],
+
+};
+export default Data;
