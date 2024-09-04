3fc4b4463a6f7c6637ac9b53f6f1327fabd1970d
Hicham <ezzhic@gmail.com>
2024-09-04 14:46:24 +0000

    fix code error

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -30,8 +30,8 @@ const Cart = () => {
                     <td>{item.price}</td>
                     <td>Quantity({item.Quantity})</td>
                     <td>
-                        <button className="btn btn-info ms-2" onClick={()=> updateItemQuantity(item.id, item.Quantity -1)}>-</button>
-                        <button className="btn btn-info ms-2" onClick={()=> updateItemQuantity(item.id, item.Quantity +1)}>+</button>
+                        <button className="btn btn-info ms-2" onClick={()=> updateItemQuantity(item.id, item.quantity - 1)}>-</button>
+                        <button className="btn btn-info ms-2" onClick={()=> updateItemQuantity(item.id, item.quantity + 1)}>+</button>
                         <button className="btn btn-danger ms-2" onClick={()=> removeItem(item.id)}>Remove Item</button>
                     </td>
                     </tr>
